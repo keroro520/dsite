37866ba6994ac81d62616254e2497a6a5ff72612
keroroxx520@gmail.com <keroroxx520@gmail.com>
2024-07-30 16:53:13 +0800

    test: add web3url unit cases

---
+++ src/web3url/__tests__/index.test.ts
@@ -0,0 +1,26 @@
+import { getDefaultChainlist, Chain } from '../index';
+
+describe('getDefaultChainlist', () => {
+    it('returns a list of chains with correct properties', () => {
+        const chainlist = getDefaultChainlist();
+        expect(chainlist).toBeInstanceOf(Array);
+        chainlist.forEach(chain => {
+            expect(chain).toHaveProperty('id');
+            expect(chain).toHaveProperty('name');
+            expect(chain).toHaveProperty('rpcUrls');
+            expect(chain).toHaveProperty('contracts');
+        });
+    });
+
+    it('includes additional RPC URLs for Ethereum mainnet', () => {
+        const chainlist = getDefaultChainlist();
+        const ethereumChain = chainlist.find(chain => chain.id === 1);
+        expect(ethereumChain?.rpcUrls).toEqual(['https://ethereum.publicnode.com', 'https://cloudflare-eth.com']);
+    });
+
+    it('includes additional RPC URLs for ETHStorage testnet', () => {
+        const chainlist = getDefaultChainlist();
+        const ethStorageChain = chainlist.find(chain => chain.id === 3333);
+        expect(ethStorageChain?.rpcUrls).toEqual(['http://testnet.ethstorage.io:9540']);
+    });
+});
